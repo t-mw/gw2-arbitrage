3e96caec35f400fd50e263dd2d67e9745b426192
Tobias Mansfield-Williams <contact@tmwhere.com>
2020-05-21 12:30:06 +0200

    Fetch recipes

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use flate2::Compression;
 use serde::{Deserialize, Serialize};
 
 use std::fs::File;
+use std::path::Path;
 
 const MAX_PAGE_SIZE: u32 = 200;
 
@@ -21,31 +22,70 @@ struct PriceInfo {
     quantity: u32,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct Recipe {
+    id: u32,
+    #[serde(rename = "type")]
+    type_name: String,
+    output_item_id: u32,
+    output_item_count: u32,
+    time_to_craft_ms: u32,
+    disciplines: Vec<String>,
+    min_rating: u32,
+    flags: Vec<String>,
+    ingredients: Vec<RecipeIngredient>,
+    chat_link: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct RecipeIngredient {
+    item_id: u32,
+    count: u32,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let current_exe = std::env::current_exe().unwrap();
     let current_exe_dir = current_exe.parent().unwrap();
 
     let prices_path = current_exe_dir.join("prices.bin");
+    let recipes_path = current_exe_dir.join("recipes.bin");
 
-    let prices = if let Ok(file) = File::open(&prices_path) {
-        println!("Loading cached prices");
+    println!("Loading prices");
+    let prices: Vec<Price> = ensure_paginated_cache(prices_path, "commerce/prices").await?;
+    println!("Loaded {} prices", prices.len());
 
+    println!("Loading recipes");
+    let recipes: Vec<Recipe> = ensure_paginated_cache(recipes_path, "recipes").await?;
+    println!("Loaded {} recipes", recipes.len());
+
+    Ok(())
+}
+
+async fn ensure_paginated_cache<T>(
+    cache_path: impl AsRef<Path>,
+    url_path: &str,
+) -> Result<Vec<T>, Box<dyn std::error::Error>>
+where
+    T: serde::Serialize,
+    T: serde::de::DeserializeOwned,
+{
+    if let Ok(file) = File::open(&cache_path) {
         let stream = DeflateDecoder::new(file);
-        deserialize_from(stream)?
+        deserialize_from(stream).map_err(|e| e.into())
     } else {
-        let mut prices: Vec<Price> = vec![];
+        let mut items: Vec<T> = vec![];
 
         let mut page_no: u32 = 0;
         let mut page_total: u32 = 1;
 
         while page_no < page_total {
             let url = format!(
-                "https://api.guildwars2.com/v2/commerce/prices?page={}&page_size={}",
-                page_no, MAX_PAGE_SIZE
+                "https://api.guildwars2.com/v2/{}?page={}&page_size={}",
+                url_path, page_no, MAX_PAGE_SIZE
             );
 
-            println!("Fetching prices from {}", url);
+            println!("Fetching {}", url);
             let response = reqwest::get(&url).await?;
 
             let page_total_str = response
@@ -58,19 +98,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 panic!("X-Page-Total is an invalid integer: {}", page_total_str)
             });
 
-            prices.append(&mut response.json::<Vec<Price>>().await?);
+            items.append(&mut response.json::<Vec<T>>().await?);
 
             page_no += 1;
         }
 
-        let file = File::create(prices_path)?;
+        let file = File::create(cache_path)?;
         let stream = DeflateEncoder::new(file, Compression::default());
-        serialize_into(stream, &prices)?;
+        serialize_into(stream, &items)?;
 
-        prices
-    };
-
-    println!("Loaded {} prices", prices.len());
-
-    Ok(())
+        Ok(items)
+    }
 }
